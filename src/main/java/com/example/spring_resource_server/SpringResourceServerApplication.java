a0aa9fa54c3f5a036f80c2b94cb620bf5e33593e
sungbin <sblee@icentric.co.kr>
2025-01-08 15:42:34 +0900

    Initial commit

---
+++ src/main/java/com/example/spring_resource_server/SpringResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_resource_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringResourceServerApplication.class, args);
+	}
+
+}
